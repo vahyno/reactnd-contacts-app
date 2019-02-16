7e6b2a3d57042683e0fd028166e82676e6fcd87f
vahyno <vahyno@gmail.com>
2019-02-16 12:28:04 -0800

    refactor CreateContact into functional component

--- src/components/CreateContact.js
+++ src/components/CreateContact.js
@@ -1,48 +1,43 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
 import ImageInput from './ImageInput'; 
 import serializeForm from 'form-serialize';
 
 
-class CreateContact extends Component {
-    state = {
+function CreateContact (props) {
 
-    }
-
-    handleSubmit = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
         const values = serializeForm(e.target, {hash: true});
         console.log('values', values);
-        if (this.props.onCreateContact) {
-            this.props.onCreateContact(values)
+        if (props.onCreateContact) {
+            props.onCreateContact(values)
         }
     }
 
-    render(){
-        return (
-            <div>
-                <Link 
-                    className='close-create-contact'
-                    to='/'>
-                        Close
-                </Link>
-                <form 
-                    onSubmit={this.handleSubmit}
-                    className='create-contact-form'>
-                    <ImageInput 
-                        className='create-contact-avatar-input'
-                        name='avatarURL'
-                        maxHeight={64}
-                    />
-                    <div className='create-contact-details'>
-                        <input type='text' name='name' placeholder='Name'/>
-                        <input type='text' name='handle' placeholder='Handle'/>
-                        <button>Add Contact</button>
-                    </div>
-                </form>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <Link 
+                className='close-create-contact'
+                to='/'>
+                    Close
+            </Link>
+            <form 
+                onSubmit={handleSubmit}
+                className='create-contact-form'>
+                <ImageInput 
+                    className='create-contact-avatar-input'
+                    name='avatarURL'
+                    maxHeight={64}
+                />
+                <div className='create-contact-details'>
+                    <input type='text' name='name' placeholder='Name'/>
+                    <input type='text' name='handle' placeholder='Handle'/>
+                    <button>Add Contact</button>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default CreateContact;
